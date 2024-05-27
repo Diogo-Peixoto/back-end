3f2a2ddf5c249e2aec8c91855e96e72633494d65
Diogo Soares Peixoto <71359547+Diogo-Peixoto@users.noreply.github.com>
2024-05-26 21:15:29 -0300

    add

---
+++ src/server.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var fastify_1 = require("fastify");
+var articles_1 = require("./router/articles");
+var cors_1 = require("@fastify/cors");
+require("dotenv/config");
+var app = (0, fastify_1.default)();
+app.register(cors_1.default);
+app.register(articles_1.articlesRoutes, { prefix: 'articles' });
+app.listen({ port: parseInt(process.env.PORT) }).then(function () { return console.log("Server Running!"); });
