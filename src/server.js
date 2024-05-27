6f0ae18e630d57dfffe5a907cc57a6a725a02463
Diogo Soares Peixoto <71359547+Diogo-Peixoto@users.noreply.github.com>
2024-05-26 22:10:01 -0300

    fd

--- src/server.js
+++ src/server.js
@@ -7,4 +7,4 @@ require("dotenv/config");
 var app = (0, fastify_1.default)();
 app.register(cors_1.default);
 app.register(articles_1.articlesRoutes, { prefix: 'articles' });
-app.listen({ port: parseInt(process.env.PORT || 3000) }).then(function () { return console.log(`Server Running! ${process.env.PORT}`); });
+app.listen({ port: 3333 }).then(function () { return console.log(`Server Running! ${process.env.PORT}`); });
